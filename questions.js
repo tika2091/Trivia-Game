4f7879f8e8e5324a1878bf919bac3f02f9953182
tika2091 <tikapsd@gmail.com>
2017-03-14 17:21:25 -0400

    dadded git

---
+++ questions.js
@@ -0,0 +1,33 @@
+var Questions = { "question" : [
+  {
+    "question"  : "What is the capital of United states of America",
+     "choice1"  : "washington",
+     "choice2"  : "New York",
+     "choice3"  : "Washington DC",
+     "choice4"  : "California",
+     "choice5"  : "Virginia",                    
+     "correct"  : 3
+  },
+  
+  {
+    "question"  : "How many states are there in United States?",
+     "choice1"  : "20",
+     "choice2"  : "30",
+     "choice3"  : "50",
+     "choice4"  : "60",
+     "choice5"  : "35",                    
+     "correct"  : 3
+  },
+  
+  {
+    "question"  : "Who is the current persident of United States?",
+     "choice1"  : "Donald J Trump",
+     "choice2"  : "Baraka Obama",
+     "choice3"  : "Hilary Clinton",
+     "choice4"  : "Barnie Senders",
+     "choice5"  : "John Machain",                    
+     "correct"  : 1
+  },
+  
+  
+]};
